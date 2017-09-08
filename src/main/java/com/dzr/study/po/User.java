331076018c6e333af63ddc400d8a8ef8ceedee84
dingzr <dingzr@joyowo.com>
2017-09-08 15:07:53 +0800

    格式化

---
+++ src/main/java/com/dzr/study/po/User.java
@@ -0,0 +1,17 @@
+package com.dzr.study.po;
+
+import lombok.Data;
+
+/**
+ * @author dingzr
+ * @Description
+ * @ClassName User
+ * @since 2017/9/8 14:39
+ */
+
+@Data
+public class User {
+
+    private Integer id;
+
+}
