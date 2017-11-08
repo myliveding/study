7eb5e0bb8ba74a648fce9c9259605e2b7ed6530b
dingzr <dingzr@joyowo.com>
2017-11-08 17:44:31 +0800

    修改代码

--- src/test/java/com/dzr/study/Test.java
+++ src/test/java/com/dzr/study/Test.java
@@ -1,5 +1,8 @@
 package com.dzr.study;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author dingzr
  * @Description
@@ -9,5 +12,13 @@ package com.dzr.study;
 public class Test {
     public static void main(String[] args) {
         System.err.println("44444444444444444444444444444");
+
+        String a = "^[0-9a-zA-Z]{6}$";
+        Pattern pattern = Pattern.compile(a);
+        Matcher matcher = pattern.matcher("333333");
+        System.err.println(matcher.matches());
+
+        System.err.println("kk_k5=555".matches("[A-Za-z0-9_]+"));
     }
+
 }
