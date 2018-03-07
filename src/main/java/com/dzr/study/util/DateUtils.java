8145deb15a876aaa1ec1d70684bfe02790e792f9
dingzr <dingzr@joyowo.com>
2018-03-07 17:36:49 +0800

    localDateTime

--- src/main/java/com/dzr/study/util/DateUtils.java
+++ src/main/java/com/dzr/study/util/DateUtils.java
@@ -21,8 +21,21 @@ public class DateUtils {
     private static ZoneId zone = ZoneId.systemDefault();
 
 
+    /**
+     * 当前时间加减天数
+     *
+     * @param param p
+     * @return r
+     */
+    public static Integer dayPlus(Integer param) {
+        LocalDateTime tomorrow = LocalDateTime.now().plusDays(param);
+        return Integer.parseInt(String.valueOf(tomorrow.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() / 1000));
+    }
+
+
     public static void main(String[] args) {
-        System.err.println(zone);
+        //System.err.println(zone);
+        System.err.println(dayPlus(3));
     }
 
 }
