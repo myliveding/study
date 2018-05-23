ea98ae7b9d4514dda701ef25b6ddc591d6d0f443
dingzr <dingzr@joyowo.com>
2018-05-23 17:45:25 +0800

    localDateTime

--- src/main/java/com/dzr/study/util/DataUtils.java
+++ src/main/java/com/dzr/study/util/DataUtils.java
@@ -1,5 +1,8 @@
 package com.dzr.study.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description
  * @FileName DataUtils
@@ -14,6 +17,28 @@ public class DataUtils {
 
     public static boolean isNotEmpty(CharSequence cs) {
         return !isEmpty(cs);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(201712);
+        list.add(201801);
+        list.add(201905);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (i > 0) {
+                int maxYear = Integer.parseInt(list.get(i).toString().substring(0, 4));
+                int maxMonth = Integer.parseInt(list.get(i).toString().substring(4, 6));
+                int minYear = Integer.parseInt(list.get(i - 1).toString().substring(0, 4));
+                int minMonth = Integer.parseInt(list.get(i - 1).toString().substring(4, 6));
+                System.err.println(minYear + "--" + minMonth + "--" + maxYear + "--" + maxMonth);
+                if ((maxYear == minYear && maxMonth - minMonth != 1) || maxYear - minYear > 1
+                        || (maxYear - minYear == 1 && (maxMonth != 1 || minMonth != 12))) {
+                    System.err.println(true);
+                }
+            }
+        }
+
     }
 
 }
