ba76d22dd3caf34b2098ed2339747e478d488047
dingzr <dingzr@xbzoom.com>
2019-02-15 09:38:01 +0800

    最近修改

---
+++ src/main/java/com/dzr/study/config/RedisListenerContainerConfig.java
@@ -0,0 +1,29 @@
+package com.dzr.study.config;
+
+
+import com.dzr.study.framework.listener.RedisExpiredListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+
+/**
+ * @Auther: dingzr
+ * @Date: 2018/12/21 13:26
+ * @Description:
+ */
+@Configuration
+public class RedisListenerContainerConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(new RedisExpiredListener(), new PatternTopic("__keyevent@8__:expired"));
+        return container;
+    }
+
+
+}
